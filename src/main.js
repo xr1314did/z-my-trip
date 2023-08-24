a8463cc905aad7c50e36eb0943d5c17b6317a67c
noledid <647850917@qq.com>
2023-08-24 13:55:23 +0800

    搭建项目结构+css样式重置

--- src/main.js
+++ src/main.js
@@ -1,6 +1,6 @@
-import './assets/main.css'
+import { createApp } from "vue";
+import App from "./App.vue";
+import "normalize.css";
+import "./assets/css/index.css";
 
-import { createApp } from 'vue'
-import App from './App.vue'
-
-createApp(App).mount('#app')
+createApp(App).mount("#app");
